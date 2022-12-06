e9f02087c81c7b6f6ea8fce7b94aee13c0b219d0
10621223 <116579692+10621223@users.noreply.github.com>
2022-12-06 12:34:42 +0000

    mount

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'who cares'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
